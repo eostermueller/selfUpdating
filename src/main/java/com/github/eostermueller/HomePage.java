09e2ab09f2b5bcf95368fe289dd6f68e59ced565
Erik Ostermueller <eostermueller@gmail.com>
2015-09-21 05:56:19 -0500

    Sven Meier's suggestion fixed this.  Now passing an IModel that retrieves the counter instead of the counter value itself.

--- src/main/java/com/github/eostermueller/HomePage.java
+++ src/main/java/com/github/eostermueller/HomePage.java
@@ -5,6 +5,7 @@ import org.apache.wicket.util.time.Duration;
 import org.apache.wicket.ajax.AjaxSelfUpdatingTimerBehavior;
 import org.apache.wicket.markup.html.basic.Label;
 import org.apache.wicket.markup.html.WebPage;
+import org.apache.wicket.model.AbstractReadOnlyModel;
 
 public class HomePage extends WebPage {
 	private static final long serialVersionUID = 1L;
@@ -12,10 +13,28 @@ public class HomePage extends WebPage {
 	public HomePage(final PageParameters parameters) {
 		super(parameters);
 
+		
 		Object o = this.getApplication();
 		if (o instanceof WicketApplication) {
 			WicketApplication w = (WicketApplication)o;
-			add(new Label("myCounter", w.getCounter().get() ));
+			
+//			add(new Label("myCounter", w.getCounter().get() ));     		// BROKEN
+			
+			AbstractReadOnlyModel<Integer> counterModel 					// FIXED
+				= new AbstractReadOnlyModel<Integer>() {					// FIXED
+				  	@Override 												// FIXED
+		             public Integer getObject() 							// FIXED
+		             { 														// FIXED
+		                 Object o = getApplication(); 						// FIXED
+		                 if (o instanceof WicketApplication) { 				// FIXED
+		                     WicketApplication w = (WicketApplication)o; 	// FIXED
+		                     return w.getCounter().get(); 					// FIXED
+		                 } 													// FIXED
+		                 return null; 										// FIXED
+		             } 														// FIXED
+			};																// FIXED
+			add( new Label("myCounter", counterModel ) );					// FIXED
+			
 		}
 		System.out.println("Found wicket application [" + o.getClass().getCanonicalName() + "]");
 
